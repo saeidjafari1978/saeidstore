ee9ca39d097619df4b538e957a011f493750da64
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-03-14 19:53:43 +0330

    add UserProfile Models

--- orderApp/models.py
+++ orderApp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class UserProfie(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='user_avatar', null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    def __str__(self):
+        return self.user.first_name+'-'+self.user.last_name
+
+
